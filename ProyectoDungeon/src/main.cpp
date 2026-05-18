354f91750d75bd260a841318dfe92dc1c96f8ed5
Samuel Barbosa Morales <zzzdy11@gmail.com>
2026-05-18 18:06:27 -0500

    agregar jugador que se mueva

--- ProyectoDungeon/src/main.cpp
+++ ProyectoDungeon/src/main.cpp
@@ -7,6 +7,12 @@ int main() {
         "Dungeon Game"
     );
 
+    sf::RectangleShape jugador(
+        sf::Vector2f(50.f, 50.f)
+    );
+
+    jugador.setPosition(100.f, 100.f);
+
     while(window.isOpen()) {
 
         sf::Event event;
@@ -19,7 +25,25 @@ int main() {
             }
         }
 
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            jugador.move(0.f, -0.2f);
+        }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            jugador.move(0.f, 0.2f);
+        }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            jugador.move(-0.2f, 0.f);
+        }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            jugador.move(0.2f, 0.f);
+        }
+
         window.clear();
+
+        window.draw(jugador);
 
         window.display();
     }
