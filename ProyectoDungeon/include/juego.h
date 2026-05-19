46da02b5b6af6e68dec25fe323f3adb5facc3dea
Samuel Barbosa Morales <zzzdy11@gmail.com>
2026-05-19 15:55:11 -0500

    poniendo las texturas, ventana, sprites

--- ProyectoDungeon/include/juego.h
+++ ProyectoDungeon/include/juego.h
@@ -1,22 +1,42 @@
 #ifndef JUEGO_H
 #define JUEGO_H
 
-#include "inventario.h"
+#include <SFML/Graphics.hpp>
+
 #include "jugador.h"
 #include "sala.h"
+#include "inventario.h"
 
 class juego {
+
 private:
-    jugador player;
+
     Sala sala;
+
+    jugador player;
+
     Inventario inventario;
+
     int numeroSala;
 
+    sf::RenderWindow window;
+
+    sf::Texture texturaJugador;
+
+    sf::Sprite jugadorSprite;
+
+    sf::RectangleShape bloque;
+
+    sf::Font fuente;
+
+    sf::Text textoPuntos;
+
+    sf::Text textoVidas;
 public:
+
     juego();
 
     void ejecutar();
 };
 
 #endif
-
